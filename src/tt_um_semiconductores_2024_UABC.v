793bdde94f53927f0c3356debaa3e37e8688e464
Rogelio <149198850+rogeermv@users.noreply.github.com>
2024-10-07 14:00:17 -0700

    Update and rename project.v to tt_um_semiconductores_2024_UABC.v

--- src/tt_um_semiconductores_2024_UABC.v
+++ src/tt_um_semiconductores_2024_UABC.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_semiconductores_2024_UABC (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
